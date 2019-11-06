64259aa0a176359315a7a9927c5dd4f0920a210b
wyp <wypcomba@outlook.com>
2019-11-06 09:55:44 +0800

    0

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,11 @@
 '''
 @Author: your name
 @Date: 2019-11-06 09:25:46
-@LastEditTime: 2019-11-06 09:43:40
+@LastEditTime: 2019-11-06 09:55:17
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: \RealTimeDspFIA\helloworld.py
 '''
 print("hello World!")
 print("HAL9000")
-print("测试提交“)
+print("测试提交")
