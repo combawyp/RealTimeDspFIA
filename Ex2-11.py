dc64fb057aea51456cb80bf6ad9761faf7b74d77
wyp <wypcomba@outlook.com>
2019-11-07 14:43:48 +0800

    0

--- Ex2-11.py
+++ Ex2-11.py
@@ -1,7 +1,7 @@
 '''
 @Author: your name
 @Date: 2019-11-07 14:28:00
-@LastEditTime: 2019-11-07 14:36:59
+@LastEditTime: 2019-11-07 14:41:01
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: \RealTimeDspFIA\Ex2-10.py
@@ -10,6 +10,11 @@ from plot_zplane import zplane
 
 b   = [1]
 a   = [1, -1, 0.9]
+
+zplane(b,a)
+
+b   = [1,0,0,0,0,0,0,0,-1]
+a   = [1, -1]
 
 zplane(b,a)
 
