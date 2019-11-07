30b265da2f7c711e09d66bdee6bcab68617a6c81
wyp <wypcomba@outlook.com>
2019-11-07 14:39:43 +0800

    0

---
+++ Ex2-11.py
@@ -0,0 +1,16 @@
+'''
+@Author: your name
+@Date: 2019-11-07 14:28:00
+@LastEditTime: 2019-11-07 14:36:59
+@LastEditors: Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: \RealTimeDspFIA\Ex2-10.py
+'''
+from plot_zplane import zplane 
+
+b   = [1]
+a   = [1, -1, 0.9]
+
+zplane(b,a)
+
+# EOF
