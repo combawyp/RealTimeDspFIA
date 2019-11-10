128d946cc7b2fd747e56a4190f86685b8e583ec5
wuyupeng comba <wypcomba@outlook.com>
2019-11-10 19:54:27 +0800

    1

--- Ex2-15-1.py
+++ Ex2-15-1.py
@@ -6,14 +6,12 @@
 @Description: In User Settings Edit
 @FilePath: \RealTimeDspFIA\Ex2-15-1.py
 '''
-# 显示的跟 matlab 并不一致，角度偏移不一致
-# 而且当参数 a[1,-1] 时运算出错，怀疑是一个不稳定的系统参数
 from scipy import signal
 import matplotlib.pyplot
 import numpy
 
 b   = [1,0,0,0,0,0,0,0,-1]
-a   = [1, -0.9]
+a   = [1, -1]
 w,h = signal.freqz(b,a)
 
 fig, ax1 = matplotlib.pyplot.subplots()
@@ -23,8 +21,8 @@ ax1.set_ylabel('Amplitude [dB]', color='b')
 ax1.set_xlabel('Frequency [pi*rad/sample]')
 
 ax2 = ax1.twinx()
-angles = numpy.unwrap(numpy.angle(h))
-ax2.plot(w/numpy.pi,(180*(angles/numpy.pi)), 'g')
+angles = numpy.angle(h, deg=True)
+ax2.plot(w/numpy.pi, angles, 'g')
 ax2.set_ylabel('Angle (degrees)', color='g')
 ax2.grid()
 ax2.axis('tight')
